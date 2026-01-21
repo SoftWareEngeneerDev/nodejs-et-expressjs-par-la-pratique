903427a5aee7180863d13ff36bcd5f47eabfe6c6
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2026-01-21 12:11:43 +0000

    Configuration du serveur et prise en main

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT  = 3000;
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+
+app.listen(PORT, function(){
+    console.log('Listining on port 3000');
+})
