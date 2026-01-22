7874804a044aef0abf312e72c63c3f5a41c7f3dc
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2026-01-22 15:12:58 +0000

    Gestion et création des routesavec node.js

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ app.get('/', function(req, res){
     res.send('hello world');
 });
 
+app.get('/movies', function(req, res){
+    res.send('La listes des movies présente')
+})
+
+app.get('/movies/add', function(req, res){
+    res.send('Le prochain formulaire ici')
+})
+
+
 app.listen(PORT, function(){
     console.log('Listining on port 3000');
+})
+app.get('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Voici le paramètre choisi ${id}`);
 })
