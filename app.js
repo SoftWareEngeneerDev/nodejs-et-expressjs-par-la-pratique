ab263510ba35286cf9a79baf0ab170feb94c270a
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2026-01-27 15:10:08 +0000

    gestion des formulaires :création et envoie des données

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const PORT  = 3000;
 
@@ -6,22 +7,34 @@ const PORT  = 3000;
 app.use('/public', express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended : false}));
 
 app.get('/', function(req, res){
     // res.send('hello world');
     res.render('index');
 });
-
+let meilleursFilms = [];
 app.get('/movies', function(req, res){
     // res.send('La listes des movies présente')
     const title = "Les meilleurs films Burkinabè des 20 dernières années";
-    const meilleursFilms = [
+    meilleursFilms = [
         {title: 'yaaba', annee : '1998'},
         {title: 'tilè', annee : '2000'},
         {title: 'razougou', annee : '1997'},
     ];
     res.render('movies', {title : title, films: meilleursFilms});
 });
+
+app.post('/movies', (req, res) =>{
+    console.log('Le titre du film est :',req.body.titreFilme);
+    console.log('L\'année de sorite est :',req.body.anneeFilme);
+    const filmRec = {title:req.body.titreFilme, annee:req.body.anneeFilme};
+    // meilleursFilms = [...meilleursFilms, filmRec];
+    meilleursFilms.push(filmRec);
+    
+    console.log(meilleursFilms);
+    res.sendStatus(201);
+})
 
 app.get('/movies-details', function(req, res){
     // res.send('La listes des movies présente')
